69337613f20796455482714fdfc7b1a3213ab0e7
Doug MacEachern <dougm@apache.org>
2001-04-02 08:53:05 +0000

    Test.pm wrapper to run under mod_perl
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework@88631 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Apache-Test/lib/Apache/Test.pm
@@ -0,0 +1,49 @@
+package Apache::Test;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Test qw(ok);
+use Exporter ();
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(ok plan);
+our $VERSION = '0.01';
+
+#so Perl's Test.pm can be run inside mod_perl
+sub init_test_pm {
+    my $r = shift;
+
+    if (defined &Apache::RequestRec::puts) {
+        package Apache::RequestRec;
+        unless (defined &PRINT) {
+            *PRINT = \&puts;
+        }
+        tie *STDOUT, __PACKAGE__, $r;
+    }
+    else {
+        $r->send_http_header; #1.xx
+    }
+
+    $r->content_type('text/plain');
+
+    $Test::TESTOUT = \*STDOUT;
+    $Test::planned = 0;
+    $Test::ntest = 1;
+}
+
+sub plan {
+    init_test_pm(shift) if ref $_[0];
+
+    my $condition = pop @_ if ref $_[-1];
+    if ($condition and ! $condition->()) {
+        print "0..1\n";
+        exit; #XXX: Apache->exit
+    }
+
+    Test::plan(@_);
+}
+
+1;
+
+
