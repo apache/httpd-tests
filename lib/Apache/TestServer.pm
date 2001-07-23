959bd09f2f2fb64f7380d553f5d0bb51b66fc87d
Doug MacEachern <dougm@apache.org>
2001-07-23 22:35:37 +0000

    server will start much faster now with the recent apr change, so dont wait as long for server to startup
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework/Apache-Test@89670 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/TestServer.pm
+++ lib/Apache/TestServer.pm
@@ -310,7 +310,7 @@ sub start {
     $mpm = "($mpm MPM)" if $mpm;
     print "using $version $mpm\n";
 
-    my $tries = 8;
+    my $tries = 5;
 
     for (1..$tries) {
         my $pid = $self->pid;
