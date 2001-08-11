e3aaf29b7ebb5e84b6d4bd9c4d78e884e90ca5b2
Doug MacEachern <dougm@apache.org>
2001-08-11 01:11:33 +0000

    module for building/configuring c module tests
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework@90079 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Apache-Test/lib/Apache/TestConfigC.pm
@@ -0,0 +1,162 @@
+package Apache::TestConfig;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Config;
+use Apache::TestConfig ();
+use Apache::TestConfigPerl ();
+use Apache::TestTrace;
+use File::Find qw(finddepth);
+
+sub cmodule_find {
+    return unless /^mod_(\w+)\.c$/;
+    my $sym = $1;
+
+    my $dir = $File::Find::dir;
+    my $file = catfile $dir, $_;
+
+    open my $fh, $file or die "open $file: $!";
+    my $v = <$fh>;
+    if ($v =~ /^\#define\s+HTTPD_TEST_REQUIRE_APACHE\s+(\d+)/) {
+        unless ($Apache::TestConfigC::apache_rev == $1) {
+            notice "$_ requires Apache version $1, skipping.";
+            return;
+        }
+    }
+    close $fh;
+
+    push @Apache::TestConfigC::modules, {
+        name => "mod_$sym",
+        sym => "${sym}_module",
+        dir  => $dir,
+        subdir => basename $dir,
+    };
+}
+
+sub cmodules_configure {
+    my($self, $dir) = @_;
+
+    $self->{cmodules_dir} = $dir ||= catfile $self->{vars}->{top_dir}, 'c-modules';
+
+    local *Apache::TestConfigC::modules = $self->{cmodules} = [];
+    local $Apache::TestConfigC::apache_rev = $self->{server}->{rev};
+
+    finddepth(\&cmodule_find, $dir);
+
+    $self->cmodules_write_makefiles;
+    $self->cmodules_compile;
+    $self->cmodules_httpd_conf;
+}
+
+sub cmodules_makefile_vars {
+    return <<EOF;
+MAKE = $Config{make}
+EOF
+}
+
+sub cmodules_write_makefiles {
+    my $self = shift;
+
+    my $modules = $self->{cmodules};
+
+    for (@$modules) {
+        $self->cmodules_write_makefile($_);
+    }
+
+    my $file = catfile $self->{cmodules_dir}, 'Makefile';
+    open my $fh, '>', $file or die "open $file: $!";
+
+    print $fh $self->cmodules_makefile_vars;
+
+    my @dirs = map { $_->{subdir} } @$modules;
+
+    my @targets = qw(all clean);
+
+    for my $dir (@dirs) {
+        for my $targ (@targets) {
+            print $fh "$dir-$targ:\n\t-cd $dir && \$(MAKE) $targ\n\n";
+        }
+    }
+
+    for my $targ (@targets) {
+        print $fh "$targ: ", (map { "$_-$targ " } @dirs), "\n\n";
+    }
+
+    close $fh or die "close $file: $!";
+}
+
+sub cmodules_write_makefile {
+    my($self, $mod) = @_;
+
+    my $name = $mod->{name};
+    my $makefile = "$mod->{dir}/Makefile";
+    notice "writing $makefile";
+
+    open my $fh, '>', $makefile or die "open $makefile: $!";
+
+    print $fh <<EOF;
+APXS=$self->{MP_APXS}
+all: $name
+
+$name: $name.c
+	\$(APXS) -c $name.c
+
+clean:
+	-rm -rf $name.o $name.lo $name.slo $name.la .libs
+EOF
+
+    close $fh or die "close $makefile: $!";
+}
+
+sub cmodules_make {
+    my $self = shift;
+    my $targ = shift || 'all';
+
+    my $cmd = "cd $self->{cmodules_dir} && $Config{make} $targ";
+    notice $cmd;
+    system $cmd;
+}
+
+sub cmodules_compile {
+    shift->cmodules_make('all');
+}
+
+sub cmodules_httpd_conf {
+    my $self = shift;
+
+    my @args;
+
+    for my $mod (@{ $self->{cmodules} }) {
+        my $dir = $mod->{dir};
+        my $so = "$dir/.libs/$mod->{name}.so";
+
+        next unless -e $so;
+
+        $self->preamble(LoadModule => "$mod->{sym} $so");
+
+        my $cname = "$mod->{name}.c";
+        my $cfile = "$dir/$cname";
+        $self->{modules}->{$cname} = 1;
+
+        $self->add_module_config($cfile, \@args);
+    }
+
+    $self->postamble(\@args) if @args;
+}
+
+sub cmodules_clean {
+    my $self = shift;
+
+    $self->cmodules_make('clean');
+
+    for my $mod (@{ $self->{cmodules} }) {
+        my $makefile = "$mod->{dir}/Makefile";
+        notice "unlink $makefile";
+        unlink $makefile;
+    }
+
+    unlink "$self->{cmodules_dir}/Makefile";
+}
+
+1;
