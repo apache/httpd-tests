22316388d315442e8c93d672e5b62e378b7eebae
Doug MacEachern <dougm@apache.org>
2001-08-17 04:18:54 +0000

    add a bundle module for CPAN.pm install
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework@90270 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Apache-Test/lib/Bundle/ApacheTest.pm
@@ -0,0 +1,45 @@
+package Bundle::ApacheTest;
+
+$VERSION = '0.01'
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::ApacheTest - A bundle to install all Apache-Test related modules
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::ApacheTest'
+
+=head1 CONTENTS
+
+Crypt::SSLeay      - For https support
+
+Devel::CoreStack   - For getting core stack info
+
+Devel::Symdump     - For, uh, dumping symbols
+
+Digest::MD5        - Needed for Digest authentication
+
+URI                - There are URIs everywhere
+
+Net::Cmd           - For libnet
+
+MIME::Base64       - Used in authentication headers
+
+HTML::Tagset       - Needed by HTML::Parser
+
+HTML::Parser       - Need by HTML::HeadParser
+
+HTML::HeadParser   - To get the correct $res->base
+
+LWP                - For libwww-perl
+
+=head1 DESCRIPTION
+
+This bundle lists all the CPAN modules use by Apache-Test.
+
+=cut
