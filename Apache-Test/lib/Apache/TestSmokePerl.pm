cf49337c97d9e087c911ad84179d8b6b4afa3d15
Stas Bekman <stas@apache.org>
2001-12-31 08:39:48 +0000

    - add generate_script functions for autogenerated SMOKE, TEST and REPORT PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework@92686 13f79535-47bb-0310-9956-ffa450edef68

--- Apache-Test/lib/Apache/TestSmokePerl.pm
+++ Apache-Test/lib/Apache/TestSmokePerl.pm
@@ -3,12 +3,39 @@ package Apache::TestSmokePerl;
 use strict;
 use warnings FATAL => 'all';
 
+use Apache::Test ();
 use Apache::TestSmoke ();
 use ModPerl::Config ();
+
+use File::Spec::Functions qw(catfile);
 
 # a subclass of Apache::TestSmoke that configures mod_perlish things
 use vars qw(@ISA);
 @ISA = qw(Apache::TestSmoke);
+
+# generate t/SMOKE script (or a different filename) which will drive
+# Apache::TestSmokePerl
+sub generate_script {
+    my ($class, $file) = @_;
+
+    $file ||= catfile 't', 'SMOKE';
+
+    my $content = <<'EOM';
+use strict;
+use warnings FATAL => 'all';
+
+use FindBin;
+use lib "$FindBin::Bin/../Apache-Test/lib";
+use lib "$FindBin::Bin/../lib";
+
+use Apache::TestSmokePerl ();
+
+Apache::TestSmokePerl->new(@ARGV)->run;
+EOM
+
+    Apache::Test::config()->write_perlscript($file, $content);
+
+}
 
 sub build_config_as_string {
     my($self) = @_;
