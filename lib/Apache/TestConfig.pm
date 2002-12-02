fe69f7ce4cc346317e407c2bb44a091d2758e60d
Stas Bekman <stas@apache.org>
2002-12-02 16:11:48 +0000

    Extend Apache::Test to allow extra configuration files to be included at the very end of httpd.conf, when everything was loaded and configured PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework/Apache-Test@97736 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/TestConfig.pm
+++ lib/Apache/TestConfig.pm
@@ -1156,18 +1156,29 @@ sub generate_httpd_conf {
 
     $self->gendir($vars->{t_logs});
 
+    my @very_last_postamble = ();
     if (my $extra_conf = $self->generate_extra_conf) {
         for my $file (@$extra_conf) {
+            my $entry;
             if ($file =~ /\.conf$/) {
                 next if $file =~ m|/httpd\.conf$|;
-                $self->postamble(Include => qq("$file"));
+                $entry = qq(Include "$file");
             }
             elsif ($file =~ /\.pl$/) {
-                $self->postamble(PerlRequire => qq("$file"));
+                $entry = qq(PerlRequire "$file");
             }
             else {
-                # nothing yet
+                next;
             }
+
+            # put the .last includes very last
+            if ($file =~ /\.last\.(conf|pl)$/) {
+                 push @very_last_postamble, $entry;
+            }
+            else {
+                $self->postamble($entry);
+            }
+
         }
     }
 
@@ -1202,6 +1213,8 @@ sub generate_httpd_conf {
     print $out "\n";
 
     $self->postamble_run($out);
+
+    print $out join "\n", @very_last_postamble;
 
     close $in;
     close $out or die "close $conf_file: $!";
