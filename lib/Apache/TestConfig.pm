31c3a8ab7fbb1bf965aaafb24b859bd35300a4a6
Stas Bekman <stas@apache.org>
2003-12-20 07:18:47 +0000

    yet another fix for -port (when passed via %Apache::TestConfig::Argv) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework/Apache-Test@102096 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/TestConfig.pm
+++ lib/Apache/TestConfig.pm
@@ -1352,8 +1352,8 @@ sub need_reconfiguration {
     my @reasons = ();
     my $vars = $self->{vars};
 
-    if ($conf_opts->{port}) {
-        push @reasons, "'-port $conf_opts->{port}' requires reconfiguration";
+    if (my $port = $conf_opts->{port} || $vars->{port}) {
+        push @reasons, "'-port $port' requires reconfiguration";
     }
 
     my $exe = $vars->{apxs} || $vars->{httpd};
