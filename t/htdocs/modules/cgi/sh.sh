4031d1baf4f7e40c87d5805085ac38aa9d6b744e
(no author) <(no author)@unknown>
2004-08-20 18:18:41 +0000

    This commit was manufactured by cvs2svn to create tag 'APACHE_TEST_1_13'.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/tags/APACHE_TEST_1_13/perl-framework@104752 13f79535-47bb-0310-9956-ffa450edef68

--- t/htdocs/modules/cgi/sh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo Content-type: text/plain
-echo
-echo sh cgi
--- t/htdocs/php/arg.php
+++
@@ -1,5 +0,0 @@
-<?php
-	for($i=0;$i<$HTTP_SERVER_VARS["argc"];$i++) {
-                echo "$i: ".$HTTP_SERVER_VARS["argv"][$i]."\n";
-        }
-?>
