c9123e4719b27755575fec180488c582daf747c9
Philippe Chiasson <gozer@apache.org>
2004-10-22 21:08:58 +0000

    Start the 1.16 cycle
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework@105552 13f79535-47bb-0310-9956-ffa450edef68

--- Apache-Test/lib/Apache/Test.pm
+++ Apache-Test/lib/Apache/Test.pm
@@ -40,7 +40,7 @@ my @test_more_exports = grep { ! /^(ok|skip|plan)$/ } @EXPORT;
 
 %EXPORT_TAGS = (withtestmore => \@test_more_exports);
 
-$VERSION = '1.15';
+$VERSION = '1.16';
 
 %SubTests = ();
 @SkipReasons = ();
