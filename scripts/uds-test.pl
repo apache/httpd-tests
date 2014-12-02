04db408fb68c67a3dec198cca5776d5132d3c7ee
Jim Jagielski <jim@apache.org>
2014-12-02 16:39:12 +0000

    Simple UDS test script (in perl)
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1642939 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scripts/uds-test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use Socket;
+use strict;
+
+my $socket_path = '/tmp/test-ptf.sock';
+my $sock_addr = sockaddr_un($socket_path);
+socket(my $server, PF_UNIX, SOCK_STREAM, 0) || die "socket: $!";
+unlink($socket_path);
+bind($server, $sock_addr) || die "bind: $!"; 
+listen($server,1024) || die "listen: $!";
+while (accept(my $new_sock, $server)) {
+    my $data = <$new_sock>;
+	print $new_sock "HTTP/1.0 200 OK\r\n";
+	print $new_sock "Content-Type: text/html\r\n\r\n";
+	print $new_sock "<html><body><h1>Hello World</h1><pre>$data</pre></body></html>\n";
+	close $new_sock;
+}
