3039d5f2b4f585d7d21818dd21c2d3afc6df1a94
Yann Ylavic <ylavic@apache.org>
2015-10-07 10:02:00 +0000

    Fix typo (trailing semicolon to <<EOF;).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1707220 13f79535-47bb-0310-9956-ffa450edef68

--- t/htdocs/modules/h2/hello.pl
+++ t/htdocs/modules/h2/hello.pl
@@ -6,7 +6,7 @@ print "Content-Type: text/html\n";
 print "\n";
 
 my $ssl_protocol = $ENV{'SSL_PROTOCOL'};
-print <<EOF;
+print <<EOF
 <html><body>
 <h2>Hello World!</h2>
 SSL_PROTOCOL="$ssl_protocol"
