1d108680315ee74dcbd571f114e78741a51cd032
Jim Jagielski <jim@apache.org>
2017-07-06 15:10:15 +0000

    make easier to uncomment out
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1801068 13f79535-47bb-0310-9956-ffa450edef68

--- t/htdocs/modules/lua/setheaders.lua
+++ t/htdocs/modules/lua/setheaders.lua
@@ -1,8 +1,8 @@
 function handle(r)
     r.headers_out["X-Header"] = "yes"
     r.headers_out["X-Host"]   = r.headers_in["Host"]
+    r.headers_out["X-Compat"]   = "compat"
     --[[
     apr_table.set(r.headers_out, "X-Compat", "compat")
     --]]
-    r.headers_out["X-Compat"]   = "compat"
 end
