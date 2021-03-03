333e87990bcf0212f7851986b83436431b69c44b
Joe Orton <jorton@apache.org>
2021-03-03 08:52:18 +0000

    Switch to using CentOS image from quay.io for redis testing, to avoid docker hub pull rate limiting.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1887133 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/redis-init.sh
+++ scripts/redis-init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 DOCKER=${DOCKER:-`which docker 2>/dev/null || which podman 2>/dev/null`}
-IMG=docker.io/redis
-
-${DOCKER} pull ${IMG}
-${DOCKER} run -d -p 6379:6379 ${IMG}
+${DOCKER} build -t httpd_redis - <<EOF
+FROM quay.io/centos/centos:8
+RUN yum install -y redis
+CMD /usr/bin/redis-server
+EOF
+${DOCKER} run -d -p 6379:6379 httpd_redis
